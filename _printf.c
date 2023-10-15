6ae400feeb1d2af996f7200324d4bf1b54497da1
AyaG96 <aya.gamal.zaher@gmail.com>
2023-10-15 16:01:13 -0700

    added

---
+++ _printf.c
@@ -0,0 +1,55 @@
+#include "main.h"
+/**
+ * _printf - produces output according to a format
+ *
+ * @format: format string
+ *
+ * Return: the number of characters printed
+ */
+
+int _printf(const char *format, ...);
+{
+	va_list args;
+
+	va_start(args, format);
+
+	while (*format != '\0')
+	{
+		if (*format == '%')
+		{
+			format++;
+			switch (*format)
+			{
+				case 'c':
+					{
+						char c = va_arg(args, int);
+
+						write(1, &c, 1);
+						break;
+					}
+				case 's':
+					{
+						char *s = va_arg(args, char *);
+
+					if (s != NULL)
+						write(1, s, strlen(s));
+					else
+						write(1, "(NULL)", 6);
+						break;
+					}
+				case'%':
+					{
+						write(1, "%", 1);
+						break;
+					}
+			}
+		}
+		else
+		{
+			write(1, format, 1);
+		}
+		format++;
+	}
+	va_end(args);
+	return (0);
+}
